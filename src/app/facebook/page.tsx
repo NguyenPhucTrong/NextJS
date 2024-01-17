4062f6d91ee754f3c5ea0a24736e0acd960214ad
NguyenPhucTrong <tronga7z6@gmail.com>
2024-01-17 22:37:12 +0700

    update 1

--- src/app/facebook/page.tsx
+++ src/app/facebook/page.tsx
@@ -1,5 +1,15 @@
 const facebook = () => {
-  return <div>facebook page</div>;
+  const handleButton = () => {
+    alert("me");
+  };
+  return (
+    <div>
+      facebook page
+      <div>
+        <button onClick={() => handleButton()}>Back Home</button>
+      </div>
+    </div>
+  );
 };
 
 export default facebook;
