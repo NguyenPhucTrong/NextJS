dc023d6d2de1e2b128702d2a3e32ac593c0bcece
NguyenPhucTrong <tronga7z6@gmail.com>
2024-01-19 07:22:23 +0700

    Boottrasp

--- src/app/facebook/page.tsx
+++ src/app/facebook/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { Button } from "react-bootstrap";
 
 const facebook = () => {
   const router = useRouter();
@@ -11,6 +12,7 @@ const facebook = () => {
     <div>
       facebook page
       <div>
+        <Button variant="primary">Text</Button>
         <button onClick={() => handleButton()}>Back Home</button>
       </div>
     </div>
