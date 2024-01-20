079f12c9318574e5473c07c79f65e32dd6b78876
NguyenPhucTrong <tronga7z6@gmail.com>
2024-01-20 21:33:18 +0700

    bootstrap

--- src/app/facebook/page.tsx
+++ src/app/facebook/page.tsx
@@ -2,21 +2,53 @@
 
 import { useRouter } from "next/navigation";
 import { Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
-const facebook = () => {
+const Facebook = () => {
   const router = useRouter();
   const handleButton = () => {
     router.push("/");
   };
   return (
-    <div>
-      facebook page
-      <div>
-        <Button variant="primary">Text</Button>
+    // <div>
+    //   facebook page
+    //   <div>
+    //     <Button variant="primary">Text</Button>
+    //     {/* <Button onClick={() => handleButton()} variant="primary">
+    //       Text1
+    //     </Button> */}
+    //     <button onClick={() => handleButton()}>Back Home</button>
+    //   </div>
+    // </div>
+
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
         <button onClick={() => handleButton()}>Back Home</button>
-      </div>
-    </div>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export default facebook;
+export default Facebook;
